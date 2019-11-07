59f763d14f1bb115f5f3c97b5bffe0a93077adb3
JosemarRocha <josemarrochads@gmail.com>
2019-11-07 10:49:48 -0400

    Add files via upload

---
+++ Python/example.asm
@@ -0,0 +1,15 @@
+     lw   $t0, 0($gp)          ;fetch i
+     slti  $t1, $t0, 3          ;i<3?
+     beq   $t1, $zero, def      ;no, -> default
+     sll   $t0, $t0, 2          ;turn i into a byte offset
+     add   $t2, $t0, $gp
+     lw    $t2, 1064($t2)       ;fetch the branch table entry
+     jr    $t2                  ;go...
+is0:
+    sw    $zero, 28($gp)       ;A[0] = 0
+is2:
+    addi  $t0, $zero, 1        ;= 1
+     sw    $t0, 32($gp)         ;A[1] = 1
+def:
+    addi  $t0, $zero, 1       ;= -1
+     sw    $t0, 28($gp)         ;A[0] = -1                          ;length of our dear string
